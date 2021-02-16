e678485724b0ac805d99bb7d67c7c315739cde92
acnaman <acnadin01@gmail.com>
2021-02-16 22:14:30 +0900

    任意のステータスコードを返すAPIを実装

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/api/", ProvideStatusCode)
 	http.ListenAndServe(":8080", nil)
+}
+
+func ProvideStatusCode(w http.ResponseWriter, r *http.Request) {
+	str := strings.TrimPrefix(r.URL.Path, "/api/")
+	code, err := strconv.Atoi(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.WriteHeader(code)
 }
